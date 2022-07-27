c10f4c993c542bff33ea7c271488558bf92360ff
Кирилл Ёж <33261991+kirillezh@users.noreply.github.com>
2022-07-27 18:46:52 +0300

    Add Chat Action

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from telethon.tl.types import Message
 #import aiogram
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.input_media import *
-from aiogram.types import ContentType, Message
+from aiogram.types import ContentType, Message, ChatActions
 
 import logging, random
 from array import *
@@ -32,6 +32,7 @@ dp = Dispatcher(bot)
 async def gdz(message: types.Message):   
     text = to_sym(message.text)
     if len(text) != 0:
+        await bot.send_chat_action(message.chat.id, 'typing')
         try:
             nomer = int(text[0])
             await bot.send_photo(chat_id=message.chat.id, photo="https://vshkole.com.ua/images/gdz/9-klas/ukrainska-mova/glazova/"+str(nomer)+".jpg", caption=end, parse_mode="HTML", reply_to_message_id=message.message_id)
@@ -42,6 +43,7 @@ async def gdz(message: types.Message):
 #TREVOGA
 @client.on(events.NewMessage(chats=(channel_name)))
 async def trevoga(message):
+    await bot.send_chat_action(message.chat.id, 'upload_photo')
     if(trevog_i in str(message.message)):
         photo = open(screenshot(), 'rb')
         await bot.send_photo(chat_id=CHAT_ID, photo=photo, caption=random.sample(master_xuilo, k=1)[0]+end, parse_mode="HTML")
@@ -53,6 +55,7 @@ async def trevoga(message):
 #Help
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):   
+    await bot.send_chat_action(message.chat.id, 'typing')
     await message.reply(help,parse_mode="HTML", disable_web_page_preview=True)
 
 
@@ -63,20 +66,26 @@ async def pox(message: types.Message):
     try:
         if(message.reply_to_message.from_user.id!=NOBOT):
             if(random.randint(1,5)==1):
+                await bot.send_chat_action(message.chat.id, 'record_voice')
+                sleep(2)
                 await bot.send_voice(chat_id=message.chat.id, voice=open('1.ogg', 'rb'),  caption=end, parse_mode="HTML")                
             else:
+                await bot.send_chat_action(message.chat.id, 'choose_sticker')
                 await message.reply_to_message.answer_sticker(mes, reply=message.reply_to_message)
         else:
             await message.answer("Ты кринж "+end, parse_mode="HTML", disable_web_page_preview=True)
     except:
         if(random.randint(1,5)==1):
-                await bot.send_voice(chat_id=message.chat.id, voice=open('1.ogg', 'rb'),  caption=end, parse_mode="HTML")                
+            await bot.send_chat_action(message.chat.id, 'record_voice')
+            await bot.send_voice(chat_id=message.chat.id, voice=open('1.ogg', 'rb'),  caption=end, parse_mode="HTML")                
         else:
+            await bot.send_chat_action(message.chat.id, 'choose_sticker')
             await message.answer_sticker(mes)
 
 #Поебать
 @dp.message_handler(commands=['poebat'])
 async def poebat(message: types.Message):   
+    await bot.send_chat_action(message.chat.id, 'typing')
     try:
         if(message.reply_to_message.from_user.id!=NOBOT):
             await message.reply_to_message.answer(poebat_+end, reply=message.reply_to_message, parse_mode="HTML", disable_web_page_preview=True)
@@ -87,6 +96,7 @@ async def poebat(message: types.Message):
 #Ливни
 @dp.message_handler(commands=['livni'])
 async def livni(message: types.Message):   
+    await bot.send_chat_action(message.chat.id, 'typing')
     mes = livni_i+end
     try:
         if(message.reply_to_message.from_user.id!=NOBOT):
@@ -100,30 +110,36 @@ async def livni(message: types.Message):
 #Cheerful check on the bot
 @dp.message_handler(commands=['bot', 'Кто бот?'])
 async def botik(message: types.Message):   
+    await bot.send_chat_action(message.chat.id, 'typing')
     await message.reply(random.choices(name, weights=(3,4,2,1,1), k=1)[0]+" бот"+end, parse_mode="HTML", disable_web_page_preview=True)
 
 #Stand with Ukraine * Fuck putin!
 @dp.message_handler(commands=['putin', 'Кто путин?'])
 async def xuilo(message: types.Message):
+    await bot.send_chat_action(message.chat.id, 'typing')
     await message.reply(random.sample(master_xuilo, k=1)[0]+end, parse_mode="HTML", disable_web_page_preview=True)
 
 #HANDLE
 @dp.message_handler(commands=['kerilhuesos', 'Ответ'])
 async def reply(message: types.Message):
+    await bot.send_chat_action(message.chat.id, 'typing')
     await message.reply("Сам хуесос!"+end, parse_mode="HTML", disable_web_page_preview=True)
 
 #GIF
 @dp.message_handler(commands=['animal', 'gif'])
 async def gif(message: types.Message):
+    await bot.send_chat_action(message.chat.id, 'choose_sticker')
     await message.answer_sticker(random.choices(sticker_id, k=1)[0], reply=message.message_id)
 # Shut up
 @dp.message_handler(commands=['close', 'Закрыть тему'])
 async def close(message: types.Message):
+    await bot.send_chat_action(message.chat.id, 'typing')
     await message.reply(random.sample(close_i, k=1)[0]+' '+end, parse_mode="HTML", disable_web_page_preview=True)
 
 #SCREENSHOT(ONLY TO @ezh_off)
 @dp.message_handler(commands=['screenshot', 'Скриншот'])
 async def screen(message: types.Message):
+    await bot.send_chat_action(message.chat.id, 'upload_photo')
     if(message.from_user.id != NOBOT):
         await message.reply(suka+end, parse_mode="HTML", disable_web_page_preview=True)
     else:
@@ -138,6 +154,7 @@ async def AMC(message: types.Message):
 
 @dp.message_handler(content_types=[ContentType.NEW_CHAT_MEMBERS])
 async def new_members_handler(message: Message):
+    await bot.send_chat_action(message.chat.id, 'typing')
     new_member = message.new_chat_members[0]
     await bot.send_message(message.chat.id, f"Привет, {new_member.mention}. Ты бот!")
 
