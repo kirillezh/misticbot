d79b00360785995e2ac9b3e17fdaaf1a00e7bfb9
Кирилл Ёж <33261991+kirillezh@users.noreply.github.com>
2022-08-08 01:36:16 +0300

    Change method localisation, optimize code

--- function.py
+++ function.py
@@ -7,113 +7,99 @@ import datetime, random
 from roughfilter import search_obscene_words
 from time import sleep
 
-from example import DRIVER, CHAT_ID, FUCK_YOU, GMT, URL_TREVOG, OFFMAT, end, NOBOT
-
+from example import DRIVER, CHAT_ID, GMT, URL_TREVOG, NOBOT, localisation
 
 from bd import DataBase
 
-#check nomer in text
+class Function:
+    #happy birthday
+    async def HappyBirthday(self, bot):   
+        nowdate = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(seconds=GMT*3600))).strftime('%d.%m.%Y')
+        info = DataBase().find(nowdate)
+        for i in info:
+            DataBase().update(i[0])
+            if(i[2]==None):
+                await bot.send_message(CHAT_ID, f"{localisation['hb1']} \n{i[3]}, {localisation['hb2']}!) \n{localisation['end']}")
+            else:
+                await bot.send_message(CHAT_ID,f"{localisation['hb3']} \n<a href='tg://user?id={i[2]}'>{i[3]}</a>, {localisation['hb2']}! \n{localisation['end']}", parse_mode="HTML")
 
-def to_sym(mes):
-    s = []
-    for t in mes.split():
+    #check mat in message    
+    async def check_mat(self, message):
+        mes=str(message.text)
+        mes=mes.lower()
         try:
-            s.append(float(t))
-        except ValueError:
-            pass
-    return s
+            ifcheck=search_obscene_words(str(message.text))
+        except:
+            ifcheck = False
+        if(ifcheck and message.from_user.id != NOBOT):
+                if('кирилл' in mes  or 'керил' in mes or 'кирил' in mes or 'крил' in mes or 'киирил' in mes):
+                    await message.reply(f"{localisation['inx']} \n{localisation['end']}",parse_mode="HTML", disable_web_page_preview=True)
+                elif(random.randint(1, 10000)==1):
+                    await message.reply(f"{localisation['offmat']} \n{localisation['end']}",parse_mode="HTML", disable_web_page_preview=True)
 
-async def hw(bot):   
-    nowdate = datetime.datetime.now().strftime('%d.%m.%Y')
-    info = DataBase().find(nowdate)
-    for i in info:
-        DataBase().update(i[0])
-        if(i[2]==None):
-            await bot.send_message(CHAT_ID, f"Сегодня День Варенья у кого-то... \n{i[3]}, с Днём Рождения!)")
+    #checking and sending tiktok-video
+    async def checker_tiktok(self, bot, message):
+        url_light=self.text_to_url(message.text)
+        if("tiktok.com/" in url_light):
+            await bot.send_chat_action(message.chat.id, 'upload_video')
+            video_data=self.url_to_video(url_light)
+            try:
+                await bot.send_video(message.chat.id, video_data['url'], reply_to_message_id=message.message_id, caption=localisation['end'],parse_mode="HTML")
+            except:
+                await message.reply(f"{localisation['bag']} \n{localisation['end']}", parse_mode="HTML", disable_web_page_preview=True)
+
+    #work with screenshot
+    def screenshot(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--disable-setuid-sandbox")
+        driver = webdriver.Chrome(DRIVER, chrome_options=chrome_options)
+        driver.set_window_size(1680, 1200)
+        driver.get(URL_TREVOG) 
+
+        time_h = getattr(datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(seconds=GMT*3600))), 'hour')
+        
+        if(time_h>=22 or time_h<=8):
+            try:
+                WebDriverWait(driver, timeout=10).until(lambda d: d.execute_script("arguments[0].setAttribute('class','dark menu-hidden')", driver.find_element(By.TAG_NAME, 'html')))
+            except:
+                pass
         else:
-            await bot.send_message(CHAT_ID,f"Оу.... Сегодня День Рождения у кого-то... \n<a href='tg://user?id={i[2]}'>{i[3]}</a>, c Днём Рождения!", parse_mode="HTML")
+            try:
+                WebDriverWait(driver, timeout=10).until(lambda d: d.execute_script("arguments[0].setAttribute('class','light menu-hidden')", driver.find_element(By.TAG_NAME, 'html')))
+            except:
+                pass
+        driver.save_screenshot("screenshot.png")
+        driver.quit()
+        return 'screenshot.png'
 
-
-
-#check mat in message    
-async def check_mat(message):
-    mes=str(message.text)
-    mes=mes.lower()
-    try:
-        ifcheck=search_obscene_words(str(message.text))
-    except:
-        ifcheck = False
-    if(ifcheck and message.from_user.id != NOBOT):
-            if('кирилл' in mes  or 'керил' in mes or 'кирил' in mes or 'крил' in mes or 'киирил' in mes):
-                await message.reply(FUCK_YOU+end,parse_mode="HTML", disable_web_page_preview=True)
-            elif(random.randint(1, 10000)==1):
-                await message.reply(OFFMAT+end,parse_mode="HTML", disable_web_page_preview=True)
-
-#checking and sending tiktok-video
-async def checker_tiktok(bot, message):
-    
-    url_light=text_to_url(message.text)
-    if("tiktok.com/" in url_light):
-        await bot.send_chat_action(message.chat.id, 'upload_video')
-        video_data=url_to_video(url_light)
+    #search url from text
+    def text_to_url(text):
+        split_text = text.split()
+        for i in split_text:
+            if "http" in i:
+                link = i
         try:
-            await bot.send_video(message.chat.id, video_data['url'], reply_to_message_id=message.message_id, caption=end,parse_mode="HTML")
+            return link
         except:
-            await message.reply('Ошибка! Дам, это баг:(' + end, parse_mode="HTML", disable_web_page_preview=True)
+            return ''
 
-#work with screenshot
-def screenshot():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument("--disable-setuid-sandbox")
-    driver = webdriver.Chrome(DRIVER, chrome_options=chrome_options)
-    driver.set_window_size(1680, 1200)
-    driver.get(URL_TREVOG) 
 
-    time_h = getattr(datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(seconds=GMT*3600))), 'hour')
-    
-    if(time_h>=22 or time_h<=8):
+    #search video from url
+    def url_to_video(url):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--kiosk")
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(executable_path=DRIVER,options=chrome_options)
+        driver.get(url) 
+        video = dict()
+        sleep(2)
         try:
-            WebDriverWait(driver, timeout=10).until(lambda d: d.execute_script("arguments[0].setAttribute('class','dark menu-hidden')", driver.find_element(By.TAG_NAME, 'html')))
+            video['url'] = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/div/div/div/video').get_attribute('src'))
         except:
-            pass
-    else:
-        try:
-            WebDriverWait(driver, timeout=10).until(lambda d: d.execute_script("arguments[0].setAttribute('class','light menu-hidden')", driver.find_element(By.TAG_NAME, 'html')))
-        except:
-            pass
-    driver.save_screenshot("screenshot.png")
-    driver.quit()
-
-    return 'screenshot.png'
-
-#search url from text
-def text_to_url(text):
-    split_text = text.split()
-    for i in split_text:
-        if "http" in i:
-            link = i
-    try:
-        return link
-    except:
-        return ''
-
-
-#search video from url
-def url_to_video(url):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument("--kiosk")
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(executable_path=DRIVER,options=chrome_options)
-    driver.get(url) 
-    video = dict()
-    sleep(2)
-    try:
-        video['url'] = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(by=By.XPATH, value='/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/div/div/div/video').get_attribute('src'))
-    except:
-        video['url'] = ''
-    driver.quit()
-    return video
+            video['url'] = ''
+        driver.quit()
+        return video
--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from aiogram.types import ContentType, Message
 import logging, random
 from array import *
 
-from example import API_ID, API_HASH, CHAT_ID, NOBOT, CHANNEL, API_TOKEN, sticker_id_pox, poebat_, trevog_i, otboi_i, close_i, livni_i, suka, sticker_id, name, otboi, testing_inform, master_xuilo, end, help, exit_
+from example import API_ID, API_HASH, CHAT_ID, NOBOT, CHANNEL, API_TOKEN, TREVOGA, OTBOY, localisation
 
-from function import check_mat, checker_tiktok, screenshot, hw
+from function import Function
 
 #start telethon
 client = TelegramClient('progress',API_ID, API_HASH)
@@ -24,45 +24,76 @@ bot = Bot(token=API_TOKEN)
 logging.basicConfig(level=logging.INFO)
 dp = Dispatcher(bot)
 
-#TREVOGA
+#Trevoga
 @client.on(events.NewMessage(chats=(CHANNEL)))
 async def trevoga(message):
     await bot.send_chat_action(CHAT_ID, 'upload_photo')
-    if(trevog_i in str(message.message)):
-        photo = open(screenshot(), 'rb')
-        await bot.send_photo(chat_id=CHAT_ID, photo=photo, caption=random.sample(master_xuilo, k=1)[0]+end, parse_mode="HTML")
-    elif(otboi_i in str(message.message)):
-        screenshot()
-        photo = open(screenshot(), 'rb')
-        await bot.send_photo(chat_id=CHAT_ID, photo=photo, caption=otboi+end, parse_mode="HTML")
+    if(TREVOGA in str(message.message)):
+        photo = open(Function().screenshot(), 'rb')
+        await bot.send_photo(
+            chat_id=CHAT_ID, 
+            photo=photo, 
+            caption=f"{random.sample(localisation['ptn_xuilo'], k=1)[0]} \n{localisation['end']}", 
+            parse_mode="HTML")
+    elif(OTBOY in str(message.message)):
+        photo = open(Function().screenshot(), 'rb')
+        await bot.send_photo(
+            chat_id=CHAT_ID, 
+            photo=photo, 
+            caption=f"{localisation['otboy']}\n{localisation['end']}", 
+            parse_mode="HTML")
 
 #Help
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await bot.send_chat_action(message.chat.id, 'typing')
-    await message.reply(help,parse_mode="HTML", disable_web_page_preview=True)
+    await message.reply(
+        f"{localisation['start']} \n{localisation['end']}",
+        parse_mode="HTML", 
+        disable_web_page_preview=True)
 
-#Похуй
+#Poxui
 @dp.message_handler(commands=['poxui'])
 async def pox(message: types.Message):   
-    mes = random.sample(sticker_id_pox, k=1)[0]
+    mes = random.sample(localisation['sticker_pox'], k=1)[0]
     try:
         if(message.reply_to_message.from_user.id!=NOBOT):
             if(random.randint(1,5)==1):
-                await bot.send_chat_action(message.chat.id, 'record_voice')
+                await bot.send_chat_action(
+                    message.chat.id, 
+                    'record_voice')
                 sleep(2)
-                await bot.send_voice(chat_id=message.chat.id, voice=open('1.ogg', 'rb'),  caption=end, parse_mode="HTML")                
+                await bot.send_voice(
+                    chat_id=message.chat.id, 
+                    voice=open('1.ogg', 'rb'),  
+                    caption=localisation['end'], 
+                    parse_mode="HTML")                
             else:
-                await bot.send_chat_action(message.chat.id, 'choose_sticker')
-                await message.reply_to_message.answer_sticker(mes, reply=message.reply_to_message)
+                await bot.send_chat_action(
+                    message.chat.id, 
+                    'choose_sticker')
+                await message.reply_to_message.answer_sticker(
+                    mes, 
+                    reply=message.reply_to_message)
         else:
-            await message.answer("Ты кринж "+end, parse_mode="HTML", disable_web_page_preview=True)
+            await message.answer(
+                f"Ты кринж {localisation['end']}", 
+                parse_mode="HTML", 
+                disable_web_page_preview=True)
     except:
         if(random.randint(1,5)==1):
-            await bot.send_chat_action(message.chat.id, 'record_voice')
-            await bot.send_voice(chat_id=message.chat.id, voice=open('1.ogg', 'rb'),  caption=end, parse_mode="HTML")                
+            await bot.send_chat_action(
+                message.chat.id, 
+                'record_voice')
+            await bot.send_voice(
+                chat_id=message.chat.id, 
+                voice=open('1.ogg', 'rb'),  
+                caption=localisation['end'], 
+                parse_mode="HTML")                
         else:
-            await bot.send_chat_action(message.chat.id, 'choose_sticker')
+            await bot.send_chat_action(
+                message.chat.id, 
+                'choose_sticker')
             await message.answer_sticker(mes)
 
 #Поебать
@@ -71,42 +102,76 @@ async def poebat(message: types.Message):
     await bot.send_chat_action(message.chat.id, 'typing')
     try:
         if(message.reply_to_message.from_user.id!=NOBOT):
-            await message.reply_to_message.answer(poebat_+end, reply=message.reply_to_message, parse_mode="HTML", disable_web_page_preview=True)
+            await message.reply_to_message.answer(
+                f"{localisation['poebat']}\n{localisation['end']}", 
+                reply=message.reply_to_message, 
+                parse_mode="HTML", 
+                disable_web_page_preview=True)
         else:
-            await message.answer("Ты кринж "+end, parse_mode="HTML", disable_web_page_preview=True)
+            await message.answer(
+                f"{localisation['youcringe']} \n {localisation['end']}", 
+                parse_mode="HTML",
+                disable_web_page_preview=True)
     except:
-        await message.answer(poebat_ + end, parse_mode="HTML", disable_web_page_preview=True)
+        await message.answer(
+            f"{localisation['poebat']} \n{localisation['end']}", 
+            parse_mode="HTML", 
+            disable_web_page_preview=True)
 #Ливни
 @dp.message_handler(commands=['livni'])
 async def livni(message: types.Message):   
     await bot.send_chat_action(message.chat.id, 'typing')
-    mes = livni_i+end
     try:
         if(message.reply_to_message.from_user.id!=NOBOT):
-            await message.reply_to_message.answer(mes, reply=message.reply_to_message, parse_mode="HTML", disable_web_page_preview=True)
+            await message.reply_to_message.answer(
+                f"{localisation['livni']}\n{localisation['end']}", 
+                reply=message.reply_to_message, 
+                parse_mode="HTML", 
+                disable_web_page_preview=True)
         else:
-            await message.answer("Ты кринж "+end, parse_mode="HTML", disable_web_page_preview=True)
+            await message.answer(
+                f"{localisation['youcringe']} \n {localisation['end']}",
+                parse_mode="HTML", 
+                disable_web_page_preview=True)
     except:
-        await message.answer(mes, parse_mode="HTML", disable_web_page_preview=True)
+        await message.answer(
+            f"{localisation['livni']}\n{localisation['end']}", 
+            parse_mode="HTML", 
+            disable_web_page_preview=True)
         
 
 #Cheerful check on the bot
 @dp.message_handler(commands=['bot', 'Кто бот?'])
 async def botik(message: types.Message):   
-    await bot.send_chat_action(message.chat.id, 'typing')
-    await message.reply(random.choices(name, weights=(3,4,2,1,1), k=1)[0]+" бот"+end, parse_mode="HTML", disable_web_page_preview=True)
+    await bot.send_chat_action(
+        message.chat.id, 
+        'typing')
+    await message.reply(
+        f"{random.choices(localisation['name'], weights=(3,4,2,1,1), k=1)[0]} {localisation['w_bot']} \n{localisation['end']}", 
+        parse_mode="HTML", 
+        disable_web_page_preview=True)
 
 #Stand with Ukraine * Fuck putin!
 @dp.message_handler(commands=['putin', 'Кто путин?'])
 async def xuilo(message: types.Message):
-    await bot.send_chat_action(message.chat.id, 'typing')
-    await message.reply(random.sample(master_xuilo, k=1)[0]+end, parse_mode="HTML", disable_web_page_preview=True)
+    await bot.send_chat_action(
+        message.chat.id, 
+        'typing')
+    await message.reply(
+        f"{random.sample(localisation['ptn_xuilo'], k=1)[0]} \n{localisation['end']}", 
+        parse_mode="HTML", 
+        disable_web_page_preview=True)
 
 #HANDLE
 @dp.message_handler(commands=['kerilhuesos', 'Ответ'])
 async def reply(message: types.Message):
-    await bot.send_chat_action(message.chat.id, 'typing')
-    await message.reply("Сам хуесос!"+end, parse_mode="HTML", disable_web_page_preview=True)
+    await bot.send_chat_action(
+        message.chat.id, 
+        'typing')
+    await message.reply(
+        f"{localisation['youxuesos']} \n{localisation['end']}", 
+        parse_mode="HTML", 
+        disable_web_page_preview=True)
 
 #Info
 @dp.message_handler(commands=['info'])
@@ -116,47 +181,72 @@ async def info(message: types.Message):
         text = ''
         for line in lines:
             text += line
-        await message.reply(text, disable_web_page_preview=True)
+        await message.reply(
+            text, 
+            disable_web_page_preview=True)
 
 #GIF
 @dp.message_handler(commands=['animal', 'gif'])
 async def gif(message: types.Message):
-    await bot.send_chat_action(message.chat.id, 'choose_sticker')
-    await message.answer_sticker(random.choices(sticker_id, k=1)[0], reply=message.message_id)
+    await bot.send_chat_action(
+        message.chat.id, 
+        'choose_sticker')
+    await message.answer_sticker(
+        random.choices(localisation['sticker_animal'], k=1)[0], 
+        reply=message.message_id)
 # Shut up
 @dp.message_handler(commands=['close', 'Закрыть тему'])
 async def close(message: types.Message):
-    await bot.send_chat_action(message.chat.id, 'typing')
-    await message.reply(random.sample(close_i, k=1)[0]+' '+end, parse_mode="HTML", disable_web_page_preview=True)
+    await bot.send_chat_action(
+        message.chat.id, 
+        'typing')
+    await message.reply(
+        f"{random.sample(localisation['close_topic'], k=1)[0]}\n{localisation['end']}", 
+        parse_mode="HTML", 
+        disable_web_page_preview=True)
 
 #SCREENSHOT(ONLY TO @ezh_off)
 @dp.message_handler(commands=['screenshot', 'Скриншот'])
 async def screen(message: types.Message):
     await bot.send_chat_action(message.chat.id, 'upload_photo')
     if(message.from_user.id != NOBOT):
-        await message.reply(suka+end, parse_mode="HTML", disable_web_page_preview=True)
+        await message.reply(
+            f"{localisation['inx']} \n{localisation['end']}", 
+            parse_mode="HTML", 
+            disable_web_page_preview=True)
     else:
-        photo = open(screenshot(), 'rb')
-        await bot.send_photo(chat_id=message.chat.id , photo=photo, caption=testing_inform+" "+end, reply_to_message_id=message.message_id, parse_mode="HTML")
+        photo = open(Function().screenshot(), 'rb')
+        await bot.send_photo(
+            chat_id=message.chat.id , 
+            photo=photo, 
+            caption=f"{localisation['testtrevog']}\n{localisation['end']}", 
+            reply_to_message_id=message.message_id, 
+            parse_mode="HTML")
 
 #AMC(All Message Checker)
 @dp.message_handler()
 async def AMC(message: types.Message):    
-    await check_mat(message)
-    await checker_tiktok(bot, message)
-    await hw(bot)
+    await Function().check_mat(message)
+    await Function().checker_tiktok(bot, message)
+    await Function().HappyBirthday(bot)
 
 @dp.message_handler(content_types=[ContentType.NEW_CHAT_MEMBERS])
 async def new_members_handler(message: Message):
-    await bot.send_chat_action(message.chat.id, 'typing')
+    await bot.send_chat_action(
+        message.chat.id, 
+        'typing')
     new_member = message.new_chat_members[0]
-    await bot.send_message(message.chat.id, f"Привет, {new_member.mention}. Ты бот!")
+    await bot.send_message(
+        message.chat.id, 
+        f"{localisation['youbot1']}, {new_member.mention}. {localisation['youbot2']}!")
 
 
 @dp.message_handler(content_types=[ContentType.LEFT_CHAT_MEMBER])
 async def new_members_handler(message: Message):
     await bot.send_chat_action(message.chat.id, 'typing')
-    await bot.send_message(message.chat.id, exit_)
+    await bot.send_message(
+        message.chat.id, 
+        f"{localisation['leave']}\n{localisation['end']}")
 
 
 if __name__ == '__main__':
