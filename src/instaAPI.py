2d3a85cf158f381119a62ac986428370969ce072
Кирилл Ёж <33261991+kirillezh@users.noreply.github.com>
2023-05-21 20:47:35 +0300

    3.10(2)

--- src/instaAPI.py
+++ src/instaAPI.py
@@ -4,18 +4,19 @@ class instaAPI:
     def __init__(self, username, password):
         self.api = instaloader.Instaloader()
         try:
-            self.api.login(username, password)
-        except instaloader.exceptions.InstaloaderException:
-            logging.error('InstaAPI: username does not exist')
+            #self.api.login(username, password)
+            self.api.load_session_from_file(username, 'instauser')
+        except instaloader.exceptions.InstaloaderException as s:
+            logging.error('InstaAPI: username does not exist\n'+s)
             return
-        except instaloader.exceptions.BadCredentialsException:
-            logging.error('InstaAPI: password is wrong')
+        except instaloader.exceptions.BadCredentialsException as s:
+            logging.error('InstaAPI: password is wrong\n'+s)
             return
-        except instaloader.exceptions.TwoFactorAuthRequiredException:
-            logging.error('InstaAPI: need 2FA')
+        except instaloader.exceptions.TwoFactorAuthRequiredException as s:
+            logging.error('InstaAPI: need 2FA\n'+s)
             return
-        except:
-            logging.error('InstaAPI: login error')
+        except Exception as s:
+            logging.error('InstaAPI: login error\n'+s)
             return
     def searchid(self, text):
         split_text = text.split('/')
